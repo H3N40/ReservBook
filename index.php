f3d6664e8805630ebd144a8d9d82f12ef6486f2b
jhonelkin <jhonelkincandamil@gmail.com>
2024-07-31 15:11:42 -0500

    prueba 1

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document0</title>
+    <title>Document</title>
 </head>
 <body>
-
+    prueba
+    prueba 2
+    
 <</body>
 </html>
