c14609c13bf86427838fc4cd1ed263124b9005b4
jhonelkin <jhonelkincandamil@gmail.com>
2024-07-31 16:01:48 -0500

    docs

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Doc</title>
+    <title>Docts</title>
 </head>
 <body>
-    Hola Elkinzaso como andas sapoperroo
+    Hola juan como andas sapoperroo
 <</body>
 </html>
