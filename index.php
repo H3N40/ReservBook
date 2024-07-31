a0f4acd499509d6e336bfdc2f72773b09c20a87a
xHENAOx <gonzalezhenaojuanjose@gmail.com>
2024-07-31 15:58:35 -0500

    modificacion del index.php

--- index.php
+++ index.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Doc</title>
 </head>
 <body>
-    prueba
-    prueba 2
-    
+    Hola Elkinzaso como andas sapoperro
 <</body>
 </html>
