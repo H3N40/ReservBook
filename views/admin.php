1839c7e56a21b56b57ac9e91053956481b00373b
Juan José Gonzalez Henao <gonzalezhenaojuanjose@gmail.com>
2025-03-05 08:09:09 -0500

    callese zorra

--- views/admin.php
+++ views/admin.php
@@ -27,7 +27,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ReservBook</title>
+    <title>ReservBoook</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/index.css">
 </head>
