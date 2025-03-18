f05d11480b90f5fe9cf0202bf99980ba5cd736b7
Juan José Gonzalez Henao <gonzalezhenaojuanjose@gmail.com>
2025-03-18 09:48:15 -0500

    register.html y register.css listos

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
 $(document).ready(function () {
-  $("#loginForm").submit(function (event) {
-    event.preventDefault();
+    $("#loginForm").submit(function (event) {
+        event.preventDefault();
 
-    var username = $.trim($('input[name="username"]').val());
-    var password = $.trim($('input[name="password"]').val());
+        var username = $.trim($('input[name="username"]').val());
+        var password = $.trim($('input[name="password"]').val());
 
-    password = encodeURIComponent(password);
-    loginUser(username, password);
-  });
+        password = encodeURIComponent(password);
+        loginUser(username, password);
+    });
 });
 
 function loginUser(username, password) {
-  $.ajax({
-    url: "../backend/login.php",
-    method: "POST",
-    data: { username: username, password: password },
-    dataType: "json",
-    success: function (response) {
-      if (response.status === "success") {
-        window.location.href = "../views/home.php";
-      } else if (response.status === "error") {
-        alertify.error(response.message);
-        $('input[name="username"]').val("");
-        $('input[name="password"]').val("");
-      } else {
-        console.log("Otro error");
-        $("#result").html("<p>Error en la llamada AJAX</p>");
-      }
-    },
-  });
+    $.ajax({
+        url: "../backend/login.php",
+        method: "POST",
+        data: {username: username, password: password},
+        dataType: "json",
+        success: function (response) {
+            if (response.status === "success") {
+                window.location.href = "../views/home.php";
+            } else if (response.status === "error") {
+                alertify.error(response.message);
+                $('input[name="username"]').val("");
+                $('input[name="password"]').val("");
+            } else {
+                console.log("Otro error");
+                $("#result").html("<p>Error en la llamada AJAX</p>");
+            }
+        },
+    });
 }
 
 
